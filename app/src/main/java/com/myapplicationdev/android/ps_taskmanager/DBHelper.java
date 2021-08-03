2ad57c827e6ecc91a98f199fdbd80e021fce2f8e
Nix WM <16046530@myrp.edu.sg>
2021-08-03 13:43:31 +0800

    Added DBHelper.java and Updated Task.java

--- app/src/main/java/com/myapplicationdev/android/ps_taskmanager/DBHelper.java
+++ app/src/main/java/com/myapplicationdev/android/ps_taskmanager/DBHelper.java
@@ -7,8 +7,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import com.myapplicationdev.android.ps_taskmanager.Task;
-
 import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper {
