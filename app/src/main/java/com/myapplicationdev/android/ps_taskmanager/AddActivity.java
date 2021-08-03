058b44e6e04a920681c54acd020f68fc378761a8
JonathanZYZ <jonathanzengyuzhao@gmail.com>
2021-08-03 13:46:16 +0800

    Test

--- app/src/main/java/com/myapplicationdev/android/ps_taskmanager/AddActivity.java
+++ app/src/main/java/com/myapplicationdev/android/ps_taskmanager/AddActivity.java
@@ -3,12 +3,39 @@ package com.myapplicationdev.android.ps_taskmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddActivity extends AppCompatActivity {
+
+    EditText etName, etDes;
+    Button btnAdd, btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        etName = findViewById(R.id.etName);
+        etDes = findViewById(R.id.etDes);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnCancel = findViewById(R.id.btnCancel);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etName.getText();
+                etDes.getText();
+
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
